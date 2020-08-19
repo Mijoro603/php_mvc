b9a680224508bb513d284382c751c5dc42fbc9c8
hkovacs <hk@hkovacs.com>
2020-08-19 08:58:22 +0300

    implemented add post functionality

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -9,6 +9,11 @@ class Pages extends Controller
 
     public function index()
     {
+
+        if (isLoggedIn()) {
+            redirect('posts');
+        }
+
         $posts = $this->postModel->getPosts();
         $data = [
             'title' => 'welcome',
--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -21,4 +21,49 @@ class Posts extends Controller
 
         $this->view('posts/index', $data);
     }
+
+    public function add()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // sanitize POST array
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'title' => trim($_POST['title']),
+                'body' => trim($_POST['body']),
+                'user_id' => $_SESSION['user_id'],
+                'title_error' => '',
+                'body_error' => ''
+            ];
+
+            // validate data
+            if (empty($data['title'])) {
+                $data['title_error'] = 'Please enter title';
+            }
+            if (empty($data['body'])) {
+                $data['body_error'] = 'Please enter body text';
+            }
+
+            // make sure no errors
+            if (empty($data['title_error']) && empty($data['body_error'])) {
+                // validated
+                if ($this->postModel->addPost($data)) {
+                    flash('post_message', 'Post Added');
+                    redirect('posts');
+                } else {
+                    die('Something went wrong');
+                }
+            } else {
+                // load view with errors
+                $this->view('posts/add', $data);
+            }
+        } else {
+            $data = [
+                'title' => '',
+                'body' => ''
+            ];
+
+            $this->view('posts/add', $data);
+        }
+    }
 }
